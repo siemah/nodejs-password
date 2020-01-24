900e175e0b843ad694b5aae85554d881694a813d
siemah <m_hamiche@inptic.edu.dz>
2020-01-24 20:44:45 +0100

    dev:generate salt helper function

---
+++ src/__tests__/helpers/index.ts
@@ -0,0 +1,17 @@
+import { generateSalt } from "../../helpers";
+
+describe('generateSalt', () => {
+  
+  it('should return a promise', () => {
+    const p = generateSalt();
+
+    expect(p).not.toBeUndefined();
+    expect(p).not.toBeNull();
+  });
+  it('generate a salt via returning a promise resolve as string', async () => {
+    const p = await generateSalt(0);
+    console.log(p)
+    expect(typeof p).toBe('string');
+  });
+  
+})
---
+++ src/helpers/index.ts
@@ -0,0 +1,20 @@
+import crypto from 'crypto';
+
+export enum HASH_ALGO {
+  SHA256 = 'sha256',
+  SHA512 = 'sha512',
+}
+
+export const generateSalt = (length: number = 1, algo: HASH_ALGO = HASH_ALGO.SHA256): Promise<string | Error> => {
+  return new Promise((resolve, reject) => {
+    const _type = 'hex';
+    try {
+      const _hash = crypto.createHash(algo);
+      const _data = crypto.randomBytes(length).toString(_type);
+      _hash.update(_data);
+      resolve(_hash.digest(_type));
+    } catch (error) {
+      reject(new Error(error))
+    }
+  });
+}
